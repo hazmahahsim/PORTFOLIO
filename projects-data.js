d72f107bebfd99023e5049f65ae029fb2be228db
Hamza hashim <hamzahashim574@gmail.com>
2026-07-03 16:12:28 +0500

    Updated portfolio website

---
+++ projects-data.js
@@ -0,0 +1,25 @@
+const projectData = {
+
+    "GYMFIT": {
+        image: "images/gymfit.jpg",
+        live: "https://hazmahahsim.github.io/GYMFIT/",
+        description: "Modern Gym Fitness Website with responsive UI and animations.",
+        tech: ["HTML","CSS","Bootstrap","JavaScript"]
+    },
+
+
+    "portfolio": {
+        image: "images/project2.png",
+        live: "https://hazmahahsim.github.io/PORTFOLIO/",
+        description: "Travel and Tourism Website.",
+        tech: ["HTML","CSS","Bootstrap"]
+    },
+
+    "hamza": {
+        image: "images/caravan.png",
+        live: " https://hazmahahsim.github.io/hamza/",
+        description: "Personal Portfolio Website.",
+        tech: ["HTML","CSS","JavaScript"]
+    }
+
+};
